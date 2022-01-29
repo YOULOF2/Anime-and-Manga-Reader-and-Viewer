7eb4870eb54d1e39b58d3520ea2ea28f3b85455f
YOULOF2 <youlof123@gmail.com>
2022-01-29 23:35:32 +0200

    Add the manga_id, and refactor some code

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ from backend.AnimeMangaSearch import AMSApi
 from backend.MangaCollectorApi import MangaCollectorApi
 from loguru import logger
 
-logger.add(sink="logfile.log", format="{time} {level} {message}")
+logger.add(sink="logfile.log", format="{time}|{level}|{message}")
 annApi = AMSApi()
 # print(annApi.search_manga_by_name("Black Clover"))
 print(annApi.get_media_details_by_id(17059))
